e10616fb908e69570674a595eb7dbfe02bbbca51
Peter Henry <phenry@stella.home>
2015-07-29 13:57:07 -0600

    created first model and placeholder index view on main app

--- main/models.py
+++ main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Message(models.Model):
+    user = models.ForeignKey(User)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
