3af5df0140546e03dd4c4aa58f08a261d2be38d2
Peter Henry <phenry@stella.home>
2015-07-29 14:32:00 -0600

    renamed the main model and added it to admin site

--- main/models.py
+++ main/models.py
@@ -2,8 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Message(models.Model):
+class Post(models.Model):
     user = models.ForeignKey(User)
     text = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return self.text
