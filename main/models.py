58148e2250ae156ac9663b27c84e4a48f60bb132
Peter Henry <phenry@caltech.edu>
2015-07-30 14:01:58 -0600

    starting on favorites feature

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     user = models.ForeignKey(User)
     text = models.TextField()
+    favoriters = models.ManyToManyField(User, related_name='favorites', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
